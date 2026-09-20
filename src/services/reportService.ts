b6a36422b732b0de026f889f13c51404ce7527cc
Nicolas Viel <nico.viel@gmail.com>
2026-09-20 15:03:51 +0200

    template

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -4,18 +4,28 @@ import { User } from '../models/user.model.js';
 import { Account } from '../models/account.model.js';
 import { emailService } from './emailService.js';
 import puppeteer from 'puppeteer';
+import handlebars from 'handlebars';
+import fs from 'fs';
+import path from 'path';
+
+
 
 export const reportService = {
 
     createAndSendReport: async (user: User) => {
      if(user.email) {
-          const html = `
-    <h1>Rapport</h1>
-    <p>Voici votre rapport généré automatiquement.</p>
-  `;
 
   // 2) Générer le PDF
-  const pdfBuffer = await reportService.generatePdfFromHtml(html);
+  const pdfBuffer = await reportService.generatePdfFromHtml(    {
+      name: "Nicolas",
+      date: new Date().toLocaleDateString(),
+      amount: 42,
+      items: [
+    { name: "Produit A", value: 42 },
+    { name: "Produit B", value: 99 },
+    { name: "Produit C", value: 12 }
+  ]
+    });
 
   // 3) Envoyer le PDF par email
 
@@ -24,7 +34,12 @@ export const reportService = {
         }
     },
 
-generatePdfFromHtml: async (html: string) => {
+generatePdfFromHtml: async (data:any) => {
+  const templatePath = path.resolve(__dirname, '../templates/report.html');
+  const html = fs.readFileSync(templatePath, 'utf8');
+  const template = handlebars.compile(html);
+  const finalHtml = template(data);
+
     const browser = await puppeteer.launch({
       headless: 'shell',
       timeout: 60000,
@@ -33,7 +48,7 @@ generatePdfFromHtml: async (html: string) => {
 
     try {
       const page = await browser.newPage();
-      await page.setContent(html, { waitUntil: 'load' });
+      await page.setContent(finalHtml, { waitUntil: 'load' });
 
       const pdfBuffer = await page.pdf({
         format: 'A4',
@@ -46,3 +61,4 @@ generatePdfFromHtml: async (html: string) => {
     }
 }
 }
+ 
